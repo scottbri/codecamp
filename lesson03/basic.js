dd939940252283c33c3f13e84767d8511bc2a085
Scott Brightwell <scott@brightwell.org>
2019-06-06 11:36:26 -0500

    lesson 3

--- lesson03/basic.js
+++ lesson03/basic.js
@@ -1,24 +1,81 @@
-const myMessage = document.querySelector('.scriptResult');
+var myMessage = document.getElementById("scriptResult");
 
-var answer = window.prompt("Enter your name: ");
-var output = "";
 
-if ( answer == "Eleanor" ) {
-	output = output + answer + ", hi! " 
-} else if (answer == "Hannah" ) {
-	output = output + answer + ", I love that name";
-} else if (answer == "loretta") {
-	output = output + answer + ",why, thats my name too!";
-} else if (answer == "coleman" ) {
-	output = output + answer + ", yes i know, your last name is brightwell";
-} else if (answer == "elijah" ) {
-	output = output + answer + ", yes i know, your my brother";
-} else if (answer == "someone" ) {
-	output = output + answer + ", hello someone, i would love it if you stoped being vague ";
-} else if (answer == "baby shark" ) {
-	output = output + answer + ", -do do do do do baby shark-";
-} else {
-		output = output + answer + ", hi."+answer+  " I dont know you";
+var happyFace = document.getElementById("happy");
+var sadFace = document.getElementById("sad");
+//var angryFace = document.getElementById("angry");
+//var coolFace = document.getElementById("cool");
+//var surprisedFace = document.getElementById("surprised");
+
+var answer = getName();
+var output = greeting(answer);
+myMessage.innerHTML = output;
+
+happyFace.addEventListener('click', happyDo, false);
+sadFace.addEventListener('click', sadDo, false);
+//angryFace.addEventListener('click', angryDo, false);
+//coolFace.addEventListener('click', coolDo, false);
+//surprisedFace.addEventListener('click', surprisedDo, false);
+
+function getName() {
+	var name = window.prompt("What's your name? ");
+	output = ""; // reset the output text 
+	return name;
 }
-myMessage.textContent = output;
+
+function greeting(name) {
+	var message = "";
+	if ( name == "Eleanor" ) {
+		message = name + ", hi! ";
+	} else if (name == "Hannah" ) {
+		message = name + ", I love that name";
+	} else if (name == "Loretta") {
+		message = name + ",why, thats my name too!";
+	} else if (name == "Coleman" ) {
+		message = name + ", yes i know, your last name is brightwell";
+	} else if (name == "Elijah" ) {
+		message = name + ", yes i know, your my brother";
+	} else if (name == "Scott" ) {
+		message = name + ", thanks for c0d3 c4mp!";
+	} else if (name == "someone" ) {
+		message = name + ", hello someone, i would love it if you stoped being vague ";
+	} else if (name == "baby shark" ) {
+		message = name + ", -do do do do do baby shark-";
+	} else {
+			message = name + ", hi." + name +  " I dont know you";
+	}
+	return(message);
+}
+
+function happyDo() {
+	var emotion = "happy";
+	console.log(answer + " is " + emotion);
+	iAmFeeling(emotion);
+}
+function sadDo() {
+	var emotion = "sad";
+	console.log(answer + " is " + emotion);
+	iAmFeeling(emotion);
+}
+
+function iAmFeeling(feeling) {
+	var message = ""
+	switch(feeling) {
+	  case "happy":
+		message = "That's wonderful, " + answer;
+		output = output + "<br>" + message.bold();
+		myMessage.innerHTML = output;
+	    break;
+	  case "sad":
+		message = "Oh no, " + answer + ".";
+		message = message + "<br>" + "Here, have some ice cream!";
+		output = output + "<br>" + message.bold();
+		myMessage.innerHTML = output;
+	    break;
+	  default:
+		message = "That's an interesting perspective.";
+		output = output + "<br>" + message.bold();
+		myMessage.innerHTML = output;
+        }
+}
 
